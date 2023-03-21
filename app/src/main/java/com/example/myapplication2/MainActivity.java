d0ff390e9c3121dc8171a988f811bb36093ec859
atiqur rahman <abulbasharatiqur@gmail.com>
2023-03-21 21:40:52 +0600

    Read text to speech

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,46 +11,66 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText inputName,inputBuyingPrice,inputSellingPrice,inputProfitPercent, inputWeight, inputHeightFeet, inputHeightInch;
-    Button clickButton, calculationButton;
+    EditText inputName,inputBuyingPrice,inputSellingPrice,inputProfitPercent, inputWeight, inputHeightFeet, inputHeightInch,inputText;
+    Button clickButton, calculationButton,readTextButton;
     TextView displayText, displayProfit;
+
+    TextToSpeech textToSpeech;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        4. BMI calculation
-        inputHeightFeet=findViewById(R.id.heightFeet);
-        inputHeightInch=findViewById(R.id.heightInch);
-        inputWeight=findViewById(R.id.weightKg);
-        calculationButton=findViewById(R.id.calculateBMI);
-        displayText=findViewById(R.id.displayResult);
-
-        calculationButton.setOnClickListener(new View.OnClickListener() {
+        inputText=findViewById(R.id.inputText);
+        readTextButton=findViewById(R.id.readTextButton);
+        textToSpeech=new TextToSpeech(MainActivity.this, new TextToSpeech.OnInitListener(){
             @Override
-            public void onClick(View view) {
-                String SWeight,SHeightFeet, SHeightInch;
-                SWeight=inputWeight.getText().toString();
-                SHeightFeet=inputHeightFeet.getText().toString();
-                SHeightInch=inputHeightInch.getText().toString();
-                if(SWeight.length()>0&&SHeightFeet.length()>0 && SHeightInch.length()>0) {
-                    double weight, height, BMI;
-                    weight = Double.parseDouble(SWeight);
-                    height = (Double.parseDouble(SHeightFeet) * .3048) + (Double.parseDouble(SHeightInch) * .0254);
-                    BMI = weight / (height * height);
-                    if (BMI > 24) {
-                        displayText.setText("Your BMI is: " + BMI + "\nYou are Motu");
-                    } else if (BMI > 18) {
-                        displayText.setText("Your BMI is: " + BMI + "\nYou are Handsome");
-                    } else {
-                        displayText.setText("Your BMI is: " + BMI + "\nYou are chikna");
-                    }
-                }
-                else{
-                    Toast.makeText(MainActivity.this,"Please, Input fields.",Toast.LENGTH_LONG).show();
-                }
+            public void onInit(int status){
+
             }
         });
+
+        readTextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textToSpeech.speak(""+inputText.getText().toString(),TextToSpeech.QUEUE_FLUSH,null,null);
+            }
+        });
+
+
+
+//        4. BMI calculation
+//        inputHeightFeet=findViewById(R.id.heightFeet);
+//        inputHeightInch=findViewById(R.id.heightInch);
+//        inputWeight=findViewById(R.id.weightKg);
+//        calculationButton=findViewById(R.id.calculateBMI);
+//        displayText=findViewById(R.id.displayResult);
+//
+//        calculationButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String SWeight,SHeightFeet, SHeightInch;
+//                SWeight=inputWeight.getText().toString();
+//                SHeightFeet=inputHeightFeet.getText().toString();
+//                SHeightInch=inputHeightInch.getText().toString();
+//                if(SWeight.length()>0&&SHeightFeet.length()>0 && SHeightInch.length()>0) {
+//                    double weight, height, BMI;
+//                    weight = Double.parseDouble(SWeight);
+//                    height = (Double.parseDouble(SHeightFeet) * .3048) + (Double.parseDouble(SHeightInch) * .0254);
+//                    BMI = weight / (height * height);
+//                    if (BMI > 24) {
+//                        displayText.setText("Your BMI is: " + BMI + "\nYou are Motu");
+//                    } else if (BMI > 18) {
+//                        displayText.setText("Your BMI is: " + BMI + "\nYou are Handsome");
+//                    } else {
+//                        displayText.setText("Your BMI is: " + BMI + "\nYou are chikna");
+//                    }
+//                }
+//                else{
+//                    Toast.makeText(MainActivity.this,"Please, Input fields.",Toast.LENGTH_LONG).show();
+//                }
+//            }
+//        });
 
 //        3. selling price calculation from profit percentage
 //        inputBuyingPrice=findViewById(R.id.buyingPrice);
